394e9f656f189338e6f680a3db48276fd1210a66
ROBY P K <robypk93@gmail.com>
2024-01-14 15:10:35 +0530

    circular progress bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,14 @@
+import CircularProgressBar from "./Components/CircularProgressBar";
+import HeroSection from "./Components/HeroSection";
+
 export default function Home() {
   return (
     <>
-      <section className="relative bg-gradient-to-r from-blue-500 to-purple-500 text-white h-screen flex items-center justify-center">
-        <div className="text-center z-10">
-          <h1 className="text-4xl md:text-6xl font-bold mb-4">Roby</h1>
-          <p className="text-lg md:text-xl mb-8">
-            Game Developer | Creative Mind
-          </p>
-          <button className="bg-white text-blue-500 px-4 py-2 rounded-full font-semibold hover:bg-blue-500 hover:text-white">
-            View Portfolio
-          </button>
-        </div>
-
-        <div className="absolute top-1/2 right-6 transform -translate-y-1/2">
-          <div className=" w-96 h-96 rounded-full overflow-hidden border-4 border-white  shadow-black shadow-2xl">
-            <img
-              src="mypic.jpg" // Replace with the path to your photo
-              alt="Your Photo"
-              className="w-full h-full object-cover"
-            />
-          </div>
-        </div>
-      </section>
+      <HeroSection />
+      <div className="  flex items-center justify-center">
+        <CircularProgressBar progress={17} />
+        <CircularProgressBar progress={67} />
+      </div>
       <div>Roby</div>
     </>
   );
